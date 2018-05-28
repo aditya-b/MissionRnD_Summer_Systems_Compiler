1559cd36e7809b02560fd80e117f5514f001994c
krishna aditya <aditya.bulusu168@gmail.com>
2018-05-28 22:48:32 +0530

    First

---
+++ Compiler/CODE.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define E 0 
+int main()
+{
+int B;
+int A;
+int C[4];
+int D;
+scanf("%d",&AX);
+scanf("%d",&BX);
+A=AX;
+B=BX;
+CX=AX+BX;
+scanf("%d",&AX);
+DX=AX-BX;
+printf("%d",DX);
+printf("%d",CX);
+if(CX == DX)
+{
+C=CX;
+printf("%d",C);
+C=DX;
+}
+else
+{
+printf("%d",C);
+printf("%d",E);
+return 0
+}
